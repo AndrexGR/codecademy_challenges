cf65eb0036bdb1018d49f9b229dec04a446a6730
Andrés Gallardo <84551731+AndrexGR@users.noreply.github.com>
2021-12-28 03:00:48 -0300

    Challenge last step completion and code cleaning.

--- mystery-organism/main.js
+++ mystery-organism/main.js
@@ -1,16 +1,10 @@
-// "You’re part of a research team that has found a new mysterious organism at the bottom of the ocean near hydrothermal vents."
-// "Your team names the organism, Pila aequor (P. aequor), and finds that it is only comprised of 15 DNA bases."
-// "The small DNA samples and frequency at which it mutates due to the hydrothermal vents make P. aequor an interesting specimen to study."
-// "However, P. aequor cannot survive above sea level and locating P. aequor in the deep sea is difficult and expensive."
-// "Your job is to create objects that simulate the DNA of P. aequor for your research team to study."
-
-// Returns a random DNA base
+/// Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
   return dnaBases[Math.floor(Math.random() * 4)] 
 }
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
@@ -19,138 +13,120 @@ const mockUpStrand = () => {
   return newStrand
 }
 
-
-// Factory function for pAequor instances creation. here i use the "returnRandBase" function.
-const pAequorFactory = (numb, arr) => {
-  //Object creation
-  const obj = {
-    specimenNum: numb,
-    dna: arr,
+// Factory function for pAequor instances creation.
+const pAequorFactory = (id, dnaArray) => {
+  const pAequorInstance = {
+    specimenNum: id,
+    dna: dnaArray,
 
     // Requested methods
-
-    // A "Mutation method".
-    // This method can be used to randomly mutate the instance's genetic code, 1 base at a time. Each time it returns the new mutated code.
     mutate() {
+      const dnaBaseIndex = Math.floor(Math.random() * this.dna.length);
       let mutation = returnRandBase();
-      const chosenIndex = Math.floor(Math.random() * 15);
-      while (this.dna[chosenIndex] === mutation) {
-        mutation = returnRandBase();
-      };
-      this.dna[chosenIndex] = mutation;
-      console.log('(Mutation made at letter #' + (chosenIndex + 1) + ').');
+      while (mutation === this.dna[dnaBaseIndex]) {
+        mutation = returnRandBase()
+      }
+      this.dna[dnaBaseIndex] = mutation;
+      console.log(`Mutation made at position ${dnaBaseIndex + 1}.`);
       return this.dna
     },
 
-    // A "DNA comparison method".
-    // This method takes 2 instance' dna codes and evaluates the homology level between them, but log the results instead of returning them.
-    compareDNA(pAequor) {
-      console.log(`Comparing genetic codes of pA-${pAequor.specimenNum} and pA-${this.specimenNum} instances.`);
-      console.log(`pA-${this.specimenNum}'s DNA code: ` + this.dna);
-      console.log(`pA-${pAequor.specimenNum}'s DNA code: ` + pAequor.dna);
-      console.log();
-      console.log('Loging matches...');
+    compareDNA(specimen2) {
       let match = 0;
       for (let i = 0; i < this.dna.length; i++) {
-        if (this.dna[i] === pAequor.dna[i]) {
-          match++;
-          console.log(`Match found at letter #${i + 1}.`);
+        if (this.dna[i] === specimen2.dna[i]) {
+          match++
         }
       }
       match = (match / this.dna.length) * 100;
-      console.log();
-      console.log(`Analysis results: pA-${pAequor.specimenNum} shares ${match}% of pA-${this.specimenNum}'s DNA.`);
+      // console.log(`Analysis results: This specimen (pA-${this.specimenNum}) shares ${match}% of its DNA with specimen 2 (pA-${specimen2.specimenNum}).`)
+      return match
     },
 
-    // A "Survival method".
-    // This method is a boolean: It evaluates the instance's DNA base composition and returns "true" only if it has a 60% C/G content:
     willLikelySurvive() {
-      let survivalTest = 0;
+      let contentOfCG = 0;
       for (let i = 0; i < this.dna.length; i++) {
         if (this.dna[i] === 'C' || this.dna[i] === 'G') {
-          survivalTest++
+          contentOfCG++
         }
-      };
-      survivalTest = (survivalTest / this.dna.length) * 100;
-      if (survivalTest >= 60) {
+      }
+      contentOfCG = (contentOfCG / this.dna.length) * 100;
+      let survivalTreshold = 60;
+      if (contentOfCG >= survivalTreshold) {
         return true
       } else {
         return false
       }
     },
 
-    // A "Complementary strand replication method".
-    // It creates and returns a complementary strand of the instance's DNA code.
+    // First part of the Step 9 of the challenge
     complementStrand() {
-      const templateRule = 'ATCG';
-      const complementRule = 'TAGC';
-      const templateStrand = this.dna;
-      let complementStrand = [];
-      for (let letter of templateStrand) {
-        const key = templateRule.indexOf(letter);
-        complementStrand.push(complementRule[key]);
-      };
+      const standardStrand = 'ATCG';
+      const standardComplement = 'TAGC';
+      const thisStrand = this.dna;
+      const complementStrand = [];
+      for (let base of thisStrand) {
+        let i = standardStrand.indexOf(base);
+        complementStrand.push(standardComplement[i])
+      }
       return complementStrand
     }
-  };
-
-  // Finally, returning the generated object
-  return obj
+  }
+  return pAequorInstance
 }
 
-
-// A pAequor mass-creator function.
-// Here i use both the "mockUpStrand" and "pAequorFactory" functions for generating an array containing "quantity" pAequor instances.
-const createInstance = (quantity) => {
+const createArrayOfInstances = quantity => {
   const arrayOfInstances = [];
-  for (let i = 0; i < quantity; i++) {
+  for (let i = 1; i <= quantity; i++) {
     const randomDNA = mockUpStrand();
-    const instanceNumber = i + 1;
-    const instance = pAequorFactory(instanceNumber, randomDNA);
-    // Creation of instances with a likely chance of survival:
+    const instanceId = i;
+    const instance = pAequorFactory(instanceId, randomDNA);
     if (instance.willLikelySurvive()) {
       arrayOfInstances.push(instance)
     } else {
-      i = i - 1;
+      i--
     }
   }
+  console.log('pAequor instances generated');
   return arrayOfInstances
 }
 
-// My array storing the requested 30 pAequor instances. 
-myArray = createInstance(30);
-
-
-// Listing on console the results stored in "myArray"
-console.log('pAequor instances generated:')
-for (let instance of myArray) {
-  //console.log(instance);
+// My array storing the requested 30 pAequor instances
+arrayForTheTeam = createArrayOfInstances(30);
+for (let instance of arrayForTheTeam) {
   console.log(`pA-${instance.specimenNum}: ${instance.dna}.`);
 }
 console.log();
 
-// A random pick of 2 objects stored in myArray for testing functions
-const randomPick1 = myArray[Math.floor(Math.random() * myArray.length)];
-const randomPick2 = myArray[Math.floor(Math.random() * myArray.length)];
+// My function for the second part of the Step 9 of the challenge
+const findMostRelatedOfAllIn = arrayOfSpecimens => {
+  const findMostRelatedForThis = (thisInstance, array) => {
+    let mostRelatedForThisInstance = [];
+    let highestMatchValue = 0;
+    for (let specimen of array) {
+      if (specimen.specimenNum !== thisInstance.specimenNum) {
+        if (highestMatchValue < thisInstance.compareDNA(specimen)) {
+          mostRelatedForThisInstance = [thisInstance.specimenNum, specimen.specimenNum];
+          highestMatchValue = thisInstance.compareDNA(specimen)
+        }
+      }
+    }
+    const bestMatchForThisInstance = {
+      pair: mostRelatedForThisInstance,
+      score: highestMatchValue
+    }
+    return bestMatchForThisInstance
+  }
+  let bestMatchOfAll = {score: 0};
+  for (let instance of arrayOfSpecimens) {
+    let resultPair = findMostRelatedForThis(instance, arrayOfSpecimens);
+    if (bestMatchOfAll.score < resultPair.score) {
+      bestMatchOfAll = resultPair
+    }
+  }
+  return bestMatchOfAll
+}
 
-// Test for compareDNA function
-randomPick1.compareDNA(randomPick2);
-console.log();
-
-// Test for mutate funtion
-console.log(`Mutation test for: Instance pA-${randomPick1.specimenNum}.`);
-console.log('DNA code: ' + randomPick1.dna);
-console.log("Mutated DNA code: " + randomPick1.mutate());
-console.log();
-
-// Test for complementStrand function
-console.log(`Complement strand test of pA-${randomPick1.specimenNum}'s DNA (now mutated):`);
-console.log(randomPick1.complementStrand());
-
-
-
-
-
-
-
-
+let result = findMostRelatedOfAllIn(arrayForTheTeam);
+console.log(`Analysis results: The two most related specimens in the array are pA-${result.pair[0]} and pA-${result.pair[1]}.`);
+console.log(`DNA homology score of ${Math.round(result.score * 100) / 100}%.`);
