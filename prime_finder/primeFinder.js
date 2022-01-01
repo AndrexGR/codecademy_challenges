5823ecb2ca8914dc7ca02da659584696fd91b392
Andrés Gallardo <84551731+AndrexGR@users.noreply.github.com>
2022-01-01 01:05:18 -0300

    Update primeFinder.js

--- prime_finder/primeFinder.js
+++ prime_finder/primeFinder.js
@@ -5,29 +5,22 @@ function primeFinder(n) {
   if (n < 2) {
     return 'There are no prime numbers below 2.'
   }
-  const primeNumbs = [2];
+  const primeNumbs = [];
   function isPrime(num) {
-    let i = 0;
-    let loopSwitch = true;
-    while(loopSwitch) {
+    for (let i = 0; i < primeNumbs.length; i++) {
       if (num % primeNumbs[i] === 0) {
-        loopSwitch = false
-      }
-      if (num % primeNumbs[i] > 0) {
-        i++
-      }
-      // This trigers only when there are no more numbers in "primeNumbs" to try
-      if (!primeNumbs[i]) {
-        return num
+        return false
       }
     }
+    return true
   }
-  let counter = primeNumbs[primeNumbs.length - 1];
-  while (counter <= n) {
+  for (let counter = 2; counter <= n; counter++) {
     if (isPrime(counter)) {
       primeNumbs.push(counter)
     }
-    counter++
   }
   return primeNumbs
 }
+
+// Leave this so we can test your code:
+module.exports = primeFinder;
